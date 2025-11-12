d961a47ee8672c941a8c1f1e19b224d16a082664
Victoryyyyy <sabrinavictory06@gmail.com>
2025-11-12 22:47:32 +0700

    Add files via upload

---
+++ week9/activity-mandiri/js/script.js
@@ -0,0 +1,30 @@
+function fn_ValForm() {
+    var sMsg = "";
+
+    var name = document.getElementById("name").value.trim();
+    var email = document.getElementById("email").value.trim();
+    var message = document.getElementById("message").value.trim();
+
+    if (name === "") {
+        sMsg += "\n* Anda belum mengisikan nama";
+    }
+    if (email === "") {
+        sMsg += "\n* Anda belum mengisikan email";
+    } else {
+        var emailPattern = /^[a-z0-9][a-z0-9._-]*[a-z0-9]@[a-z0-9][a-z0-9.-]*[a-z0-9]\.[a-z0-9]{2,4}$/i;
+        if (!emailPattern.test(email)) {
+            sMsg += "\n* Format email tidak valid";
+        }
+    }
+    if (message === "") {
+        sMsg += "\n* Anda belum mengisikan pesan";
+    }
+
+    if (sMsg !== "") {
+        alert("Peringatan:\n" + sMsg);
+        return false;
+    } else {
+        alert("Form berhasil dikirim!");
+        return true;
+    }
+}
